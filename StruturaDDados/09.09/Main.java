46c7cc38f9aad59a6d1348ef69f278066816ddae
Ricardo Andre Lopes Ikeda <89215809+RicardoALIkeda@users.noreply.github.com>
2024-09-09 11:03:24 +0000

    oba

---
+++ StruturaDDados/09.09/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        Lista lista = new Lista(10);
+        lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
+        lista.adiciona(4);
+        lista.adiciona(5);
+        lista.adiciona(6);
+        lista.adiciona(7);
+        lista.adiciona(8);
+        lista.adiciona(9);
+        lista.adiciona(10);
+
+}

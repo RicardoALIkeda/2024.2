a3b873a4160492eca2600021f5c58eb2fd00021d
RicardoALIkeda <32158378@mackenzista.com.br>
2024-09-08 22:14:15 -0300

    ad

--- ProgSis2/08.09/Main.java
+++ ProgSis2/08.09/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-      GerenciadorNomes gNomes = new GerenciadorNomesMem();
+      GerenciadorNomes gNomes = new GerenciadorNomes();
       Ihm ihm = new Ihm(gNomes);
       ihm.dialogar();
     }

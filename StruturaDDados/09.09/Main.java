28707740499a505ad578925b420f23d944cf5b26
Ricardo Andre Lopes Ikeda <89215809+RicardoALIkeda@users.noreply.github.com>
2024-09-09 11:37:52 +0000

    MELHOR AINDA EMMMMMM

--- StruturaDDados/09.09/Main.java
+++ StruturaDDados/09.09/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Lista scores = new Lista(10);
 
-        scores.add(new Gamer("Alice", 500));
-        scores.add(new Gamer("Bob", 300));
-        scores.add(new Gamer("Charlie", 700));
-        scores.add(new Gamer("Dave", 600));
-        scores.add(new Gamer("Eve", 400));
+        scores.add(new Gamer("JOAO", 500));
+        scores.add(new Gamer("MARIA", 300));
+        scores.add(new Gamer("SILVIA", 700));
+        scores.add(new Gamer("PEDRO", 666));
+        scores.add(new Gamer("SARAH", 400));
 
         System.out.println("Lista de scores:");
         scores.mostraLista();

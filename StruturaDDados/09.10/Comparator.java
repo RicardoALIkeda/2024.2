2b69f8cfc2ba14d1f561e4bd16c193ef60bc639f
RicardoALIkeda <32158378@mackenzista.com.br>
2024-10-09 16:46:30 -0300

    ss

---
+++ StruturaDDados/09.10/Comparator.java
@@ -0,0 +1,23 @@
+
+
+@FunctionalInterface
+public interface Comparator<T> {
+    int compare(T o1, T o2);
+
+    // Métodos padrão e estáticos adicionais
+    boolean equals(Object obj);
+
+    default Comparator<T> reversed() {
+        return (T o1, T o2) -> compare(o2, o1);
+    }
+
+    static <T extends Comparable<? super T>> Comparator<T> naturalOrder() {
+        return (T o1, T o2) -> o1.compareTo(o2);
+    }
+
+    static <T extends Comparable<? super T>> Comparator<T> reverseOrder() {
+        return (T o1, T o2) -> o2.compareTo(o1);
+    }
+
+    // Outros métodos padrão e estáticos
+}
